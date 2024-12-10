02fb2889e4f3656b0cae6db9dd8efbd0d85ccf30
Hamza M <hamza-x@users.noreply.github.com>
2024-12-10 16:07:28 +0000

    Added in function to calc best params provided by Chris Coates

--- tests/test_hpl_input.py
+++ tests/test_hpl_input.py
@@ -1,6 +1,5 @@
 import unittest
-import json
-from pathlib import Path
+from parameterized import parameterized
 
 from hmxlabs.hplx.hpl_input import HplInputFileGenerator
 
@@ -15,3 +14,28 @@ class TestHplInputFileGenerator(unittest.TestCase):
 
         self.assertEqual(expected_output, output, "The generated HPL input file did not match the expected output")
 
+    def test_generate_theoretical_best_params(self) -> None:
+        cpu_count: int = 4
+        params = HplInputFileGenerator.generate_theoretical_best_inputs(4, 16)
+
+        self.assertEqual(1000, params[0], "The value of N was not as expected")
+        self.assertEqual(31, params[1], "The value of NB was not as expected")
+        self.assertEqual(2, params[2], "The value of P was not as expected")
+        self.assertEqual(2, params[3], "The value of Q was not as expected")
+
+        self.assertEqual(cpu_count, params[2]*params[3], "The value of P*Q was not as the cpu count")
+
+    @parameterized.expand([
+                            ["case1", 4, 16],
+                            ["case2", 8, 32],
+                            ["case3", 16, 64],
+                            ["case4", 32, 128],
+                            ["case5", 64, 256],
+                            ["case6", 128, 512],
+                            ["case6a", 192, 512],
+                            ["case7", 256, 1024],
+                        ])
+    def test_generate_theoretical_best_params_ext(self, _, cpu_count: int, available_memory_gb: int) -> None:
+        params = HplInputFileGenerator.generate_theoretical_best_inputs(cpu_count, available_memory_gb)
+
+        self.assertEqual(cpu_count, params[2]*params[3], "The value of P*Q was not as the cpu count")
